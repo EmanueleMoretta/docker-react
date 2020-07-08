f768201ecc6f49a6e3bc26e96aa77f5cd9fc14c7
Emanuele Moretta <emanuele.moretta@prometeia.com>
2020-07-09 00:31:13 +0200

    modified App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          master title new
+          master title newn
         </a>
       </header>
     </div>
